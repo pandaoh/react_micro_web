93a67b6554df7e9dbc9e9fa81b26f9e351ef498e
doubleam <admin@biugle.cn>
2022-07-21 18:10:40 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+/*
+ * @Author: HxB
+ * @Date: 2022-04-15 14:29:54
+ * @LastEditors: DoubleAm
+ * @LastEditTime: 2022-07-21 18:01:18
+ * @Description: eslint 配置文件
+ * @FilePath: \react_micro_web\.eslintrc.js
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'spellcheck', 'import', 'zob'],
+  rules: {
+    'no-undef': ['error'],
+    'spaced-comment': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 5 }],
+    'no-mixed-spaces-and-tabs': ['error', false],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'no-irregular-whitespace': ['error', { skipStrings: true }],
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'no-trailing-spaces': ['error', { skipBlankLines: false }],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'key-spacing': ['warn', { beforeColon: false, afterColon: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'max-lines': ['error', 800],
+    'max-statements': ['error', 100],
+    'spellcheck/spell-checker': [
+      'warn',
+      {
+        comments: false,
+        strings: true,
+        identifiers: true,
+        lang: 'en_US',
+        skipWords: [
+          'javascript',
+          'debounce',
+          'pathname',
+          'minify',
+          'charset',
+          'resize',
+          'linux',
+          'darwin',
+          'resizable',
+        ],
+        skipIfMatch: [
+          'http://[^s]*',
+          '^[-\\w]+/[-\\w\\.]+$', // For MIME Types
+        ],
+        skipWordIfMatch: [
+          '^foobar.*$', // words that begin with foobar will not be checked
+        ],
+        minLength: 5,
+      },
+    ],
+    'import/first': ['error'],
+    'import/exports-last': ['error'],
+    'import/newline-after-import': ['error'],
+    'import/no-duplicates': ['error'],
+    'import/order': ['error', { 'newlines-between': 'never' }],
+    'zob/comment': 'error', // 中英文空格间距 找英文符号正则 ([\u4e00-\u9fa5]\s*[!@#$%^&*_+=;:'"{\[\]},.\/\\`\(\)])
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # react_micro_web
-基于 React + TypeScript + Vite + Antd + Electron 实现的伪微前端模板(适用于开发大型/复杂项目但又人数较少的情况)
+
+> 基于 React + TypeScript + Vite + Antd + Electron 实现的伪微前端模板(适用于开发大型/复杂项目但又人数较少的情况)
+
+## Technology Stack
+
+* React 17.x
+* Redux Toolkit
+* TypeScript
+* Scss/Less
+* Ant Design 4.x
+* Vite 2.x
+* Electron
+* EsLint
+* Prettier
+* Husky
+
+## Use
+
+```bash
+git clone https://github.com/pandaoh/react_micro_web.git
+
+npm i -g js-xcmd
+xcmd create-micro-web [dir]
+```
+
+## Start
+
+* `npm install`
+
+## Development
+
+* `npm run dev` or `npm run dev:exe`
+
+## Preview
+
+* `npm run server` or `npm run server:exe`
+
+## Build
+
+* `npm run build` or `npm run build:exe`
+
+## Code Check
+
+* `npm run lint`
+
+## Docs
+
+* [React](https://reactjs.bootcss.com/)
+* [Redux Toolkit](http://cn.redux.js.org/redux-toolkit/overview/)
+* [Vite](https://cn.vitejs.dev/guide/)
+* [Ant Design](https://ant.design/components/overview-cn/)
+* [Ant Design ProComponents](https://procomponents.ant.design/components/)
+
+## Others
+
+* [Issue](https://github.com/pandaoh/react_micro_web/issues)
+* [Pull Request](https://github.com/pandaoh/react_micro_web/pulls)
+* [hxbpandaoh@163.com](mailto:hxbpandaoh@163.com)
+* [Blog](http://a.biugle.cn)
+* Leo He
